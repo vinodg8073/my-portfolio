71c8a94ed4276ce6ef09401458e5a43f25cb8a86
Vinod G <vinodgowda1998@gmail.com>
2023-02-05 23:57:27 +0530

    myCertificates

--- src/components/MyCertificates.vue
+++ src/components/MyCertificates.vue
@@ -1,13 +1,38 @@
 <template>
+    <div class="cert">
+        <i class="heading"><h1>Certificates : </h1></i>
+    
     <div class="certificates">
+    
     <div class="certificate-card">
         <img  class="certificate-img" src="@/assets/logo.png" alt="">
-        <!-- <h3>Java Full Stack Development Course Certificate Nov(2021) - May(2022)</h3> -->
+        <div class="details">
+            <h3>Java Full Stack Development Course Certificate Nov(2021) - May(2022)</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis quam rem doloremque dolorem nesciunt ipsum repudiandae mollitia, deserunt incidunt provident id. Nulla eaque quae veritatis!</p>
+        </div>
     </div>
     <div class="certificate-card">
         <img  class="certificate-img" src="@/assets/logo.png" alt="">
-        <!-- <h3>Java Full Stack Development Course Certificate Nov(2021) - May(2022)</h3> -->
+        <div class="details">
+            <h3>Java Full Stack Development Course Certificate Nov(2021) - May(2022)</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis quam rem doloremque dolorem nesciunt ipsum repudiandae mollitia, deserunt incidunt provident id. Nulla eaque quae veritatis!</p>
+        </div>
     </div>
+    <div class="certificate-card">
+        <img  class="certificate-img" src="@/assets/logo.png" alt="">
+        <div class="details">
+            <h3>Java Full Stack Development Course Certificate Nov(2021) - May(2022)</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis quam rem doloremque dolorem nesciunt ipsum repudiandae mollitia, deserunt incidunt provident id. Nulla eaque quae veritatis!</p>
+        </div>
+    </div>
+    <div class="certificate-card">
+        <img  class="certificate-img" src="@/assets/logo.png" alt="">
+        <div class="details">
+            <h3>Java Full Stack Development Course Certificate Nov(2021) - May(2022)</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis quam rem doloremque dolorem nesciunt ipsum repudiandae mollitia, deserunt incidunt provident id. Nulla eaque quae veritatis!</p>
+        </div>
+    </div>
+</div>
 </div>
 </template>
 
@@ -18,10 +43,36 @@
 </script>
 
 <style scoped>
- .certificate-card{
+
+.cert{
+    background-color:  rgb(245, 248, 248);
+    margin: 0px 0px;
+    padding: 20px;
+}
+.certificate-card{
     display: grid;
     grid-template: 2fr;
     grid-auto-columns: 2fr;
     /* grid-area: 2fr; */
+    padding: 10px;
+    background: #151515;
+    color: white;
+    margin: 10px 5px 10px 5px;
+    border-radius: 10px;
+ }
+
+ .certificates{
+    
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(400px, 2fr));
+ }
+
+ img{
+    margin: auto;
+ }
+
+ .details{
+    display: contents;
+    align-items: center;
  }
 </style>
